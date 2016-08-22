86adb592e579e097081ff20ffb9e36c08f67e593
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-08-22 16:42:14 +0900

    備品申請のサーブレット更新

--- src/ateam/logic/RequestLogic.java
+++ src/ateam/logic/RequestLogic.java
@@ -1,7 +1,7 @@
 package ateam.logic;
 
 public class RequestLogic {
-	public boolean requestBihin(String bihinID) {
+	public static boolean requestBihin(String bihinID) {
 	/*	if(bihin.getStatus() == 1) {
 			//ステータスが貸出可の場合にのみtureを返す
 			return true;
