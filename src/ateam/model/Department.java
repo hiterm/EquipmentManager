6e64007645f0a61161000bffb61ac892c1dd3361
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-08-22 12:20:29 +0900

    ユーザ管理と部署管理のモデルを作成しました

---
+++ src/ateam/model/Department.java
@@ -0,0 +1,31 @@
+package ateam.model;
+
+public class Department {
+	private String deptID;
+	private String deptName;
+	private String deptKana;
+
+	public String getDeptID() {
+		return deptID;
+	}
+
+	public void setDeptID(String deptID) {
+		this.deptID = deptID;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public String getDeptKana() {
+		return deptKana;
+	}
+
+	public void setDeptKana(String deptKana) {
+		this.deptKana = deptKana;
+	}
+}
