346455ba4b137fc9532c56102493ba96dbc4be3f
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-08-22 14:06:21 +0900

    備品申請の判定だけします

---
+++ src/ateam/logic/RequestLogic.java
@@ -0,0 +1,11 @@
+package ateam.logic;
+
+public class RequestLogic {
+	public boolean requestBihin() {
+	/*	if(bihin.getStatus() == 1) {
+			//ステータスが貸出可の場合にのみtureを返す
+			return true;
+		}*/
+		return true;
+	}
+}
