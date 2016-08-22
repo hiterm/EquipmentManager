48d8092007dcca7f524328863d04b005cd40ef5f
htlsne <htlsne@users.noreply.github.com>
2016-08-22 14:08:02 +0900

    update StateDisplayLogic

--- src/ateam/logic/StateDisplayLogic.java
+++ src/ateam/logic/StateDisplayLogic.java
@@ -14,20 +14,20 @@ public class StateDisplayLogic {
 		// 適当な備品を追加
 		Bihin bihin = new Bihin();
 		bihin.setBihinID("bihinID");
-		bihin.setBihinKana("ビヒンカナ");
-		bihin.setBihinName("備品名");
+		bihin.setBihinKana("ツクエ");
+		bihin.setBihinName("机");
 		bihin.setReturnDay(Date.valueOf("2016-8-30"));
 		bihin.setStatus(2);
-		bihin.setUserID("UserID");
+		bihin.setUserID("00001");
 		list.add(bihin);
 
 		Bihin bihin2 = new Bihin();
 		bihin2.setBihinID("bihin2ID");
-		bihin2.setBihinKana("ビヒンカナ2");
-		bihin2.setBihinName("備品名2");
+		bihin2.setBihinKana("ホワイトボード");
+		bihin2.setBihinName("ホワイトボード");
 		bihin2.setReturnDay(Date.valueOf("2016-9-10"));
 		bihin2.setStatus(2);
-		bihin2.setUserID("UserID2");
+		bihin2.setUserID("00002");
 		list.add(bihin);
 
 		return list;
