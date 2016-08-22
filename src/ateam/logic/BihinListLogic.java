8f833da645e907709d1f50d74723d793a71e4ba2
htlsne <htlsne@users.noreply.github.com>
2016-08-22 14:12:59 +0900

    add BihinListLogic

---
+++ src/ateam/logic/BihinListLogic.java
@@ -0,0 +1,34 @@
+package ateam.logic;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import ateam.model.Bihin;
+
+public class BihinListLogic {
+	public static List<Bihin> getAllBihinList () {
+		List<Bihin> list = new ArrayList<Bihin>();
+
+		// 適当な備品を追加
+		Bihin bihin = new Bihin();
+		bihin.setBihinID("T001");
+		bihin.setBihinKana("ツクエ");
+		bihin.setBihinName("机");
+		bihin.setReturnDay(Date.valueOf("2016-8-30"));
+		bihin.setStatus(2);
+		bihin.setUserID("00001");
+		list.add(bihin);
+
+		Bihin bihin2 = new Bihin();
+		bihin2.setBihinID("W001");
+		bihin2.setBihinKana("ホワイトボード");
+		bihin2.setBihinName("ホワイトボード");
+		bihin2.setReturnDay(Date.valueOf("2016-9-10"));
+		bihin2.setStatus(2);
+		bihin2.setUserID("00002");
+		list.add(bihin);
+
+		return list;
+	}
+}
