bc951be1d4ef9f8b13afba0e2af819ba35bc4894
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-08-22 13:08:26 +0900

    LoginLogic appdate

--- src/ateam/logic/LoginLogic.java
+++ src/ateam/logic/LoginLogic.java
@@ -2,4 +2,12 @@ package ateam.logic;
 
 public class LoginLogic {
 
+	protected boolean login(String userID, String password){
+		if(userID.equals("root") && password.equals("root") ){
+			return true;
+		}
+		return false;
+
+	}
+
 }
