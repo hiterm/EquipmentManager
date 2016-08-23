97647327bd24f7a2c233ceed5d8242cb19108439
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-08-23 13:12:38 +0900

    loginからmypageへ

---
+++ EquipmentManager/src/ateam/controller/LoginServlet.java
@@ -0,0 +1,50 @@
+package ateam.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ateam.logic.LoginLogic;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		if(LoginLogic.login(request.getParameter("userID"), request.getParameter("password"))){
+			request.getRequestDispatcher("topPage.jsp").forward(request, response);
+		}else{
+			request.getRequestDispatcher("loginFailed.jsp").forward(request, response);
+		}
+
+	}
+
+}
