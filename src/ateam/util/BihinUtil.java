d5ac609772374da6bfc795215399d9f45325ca0b
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-08-24 14:09:00 +0900

    BihinUtilの変更

--- src/ateam/util/BihinUtil.java
+++ src/ateam/util/BihinUtil.java
@@ -2,14 +2,18 @@ package ateam.util;
 
 public class BihinUtil {
     static String getStatusStr(int status){
-        String statusName;
-        if(status == 1){
-            statusName = "利用可能";
-            return statusName;
-        }else{
-            statusName = "貸出中";
-            return statusName;
+        String statusName = null;
+        switch(status){
+        case 1: statusName = "利用可能";
+        break;
+        case 2: statusName = "貸出中";
+        break;
+        case 3: statusName = "返却済み";
+        break;
+        case 4: statusName = "貸出申請中";
+        break;
         }
+        return statusName;
     }
 
 }
