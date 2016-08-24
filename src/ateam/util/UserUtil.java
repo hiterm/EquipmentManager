b97db5dc1c7032355358166437ef00e4d2b6c861
htlsne <htlsne@users.noreply.github.com>
2016-08-24 14:06:27 +0900

    Add UserUtil.getUserName

--- src/ateam/util/UserUtil.java
+++ src/ateam/util/UserUtil.java
@@ -1,5 +1,12 @@
 package ateam.util;
 
-public class UserUtil {
+import ateam.dao.UserDAO;
+import ateam.model.User;
 
+public class UserUtil {
+    public static String getUserName(String userID) {
+        UserDAO dao = UserDAO.getInstance();
+        User user = dao.getUser(userID);
+        return user.getUserName();
+    }
 }
