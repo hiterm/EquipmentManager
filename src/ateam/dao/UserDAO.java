f23986f1f7c115a4de7d37e0c5454273d49036dc
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-08-24 13:35:05 +0900

    修正

--- src/ateam/dao/UserDAO.java
+++ src/ateam/dao/UserDAO.java
@@ -18,11 +18,15 @@ public class UserDAO {
 
 	public User getUser(String userID) {
 		String sql = "SELECT *  FROM UserKanri WHERE userID = '" + userID +"';";
+		if(sql != null){
 		try {
 			return DBManager.getObject(sql , new UserBeansMapping());
 		}
 		catch (SQLException e) {
 			throw new IllegalStateException(e);
+		}
+		}else{
+        return null;
 		}
 	}
 }
