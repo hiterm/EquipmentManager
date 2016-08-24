5301437aa65486201572da56e7faa6250ac8cefa
htlsne <htlsne@users.noreply.github.com>
2016-08-24 10:55:36 +0900

    BihinSearchLogicでimport BihinDAO

--- src/ateam/logic/BihinSearchLogic.java
+++ src/ateam/logic/BihinSearchLogic.java
@@ -2,11 +2,12 @@ package ateam.logic;
 
 import java.util.List;
 
+import ateam.dao.BihinDAO;
 import ateam.model.Bihin;
 
 public class BihinSearchLogic {
 	public static List<Bihin> getBihinSearchList(String id) {
-		ateam.dao.BihinDAO dao = ateam.dao.BihinDAO.getInstance();
+		ateam.dao.BihinDAO dao = BihinDAO.getInstance();
 		List <Bihin> list = dao.getBihinList(id);
 
 		return list;
