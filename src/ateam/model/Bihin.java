504d0e221450eeecccfcbbd4b9a5973cd9c30fae
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-08-24 14:35:42 +0900

    modelのフォーマット

--- src/ateam/model/Bihin.java
+++ src/ateam/model/Bihin.java
@@ -3,58 +3,58 @@ package ateam.model;
 import java.sql.Date;
 
 public class Bihin {
-	private String bihinID;
-	private String bihinName;
-	private String bihinKana;
-	private int status;
-	private String userID;
-	private Date returnDay;
+    private String bihinID;
+    private String bihinName;
+    private String bihinKana;
+    private int status;
+    private String userID;
+    private Date returnDay;
 
-	public String getBihinID() {
-		return bihinID;
-	}
+    public String getBihinID() {
+        return bihinID;
+    }
 
-	public void setBihinID(String bihinID) {
-		this.bihinID = bihinID;
-	}
+    public void setBihinID(String bihinID) {
+        this.bihinID = bihinID;
+    }
 
-	public String getBihinName() {
-		return bihinName;
-	}
+    public String getBihinName() {
+        return bihinName;
+    }
 
-	public void setBihinName(String bihinName) {
-		this.bihinName = bihinName;
-	}
+    public void setBihinName(String bihinName) {
+        this.bihinName = bihinName;
+    }
 
-	public String getBihinKana() {
-		return bihinKana;
-	}
+    public String getBihinKana() {
+        return bihinKana;
+    }
 
-	public void setBihinKana(String bihinKana) {
-		this.bihinKana = bihinKana;
-	}
+    public void setBihinKana(String bihinKana) {
+        this.bihinKana = bihinKana;
+    }
 
-	public int getStatus() {
-		return status;
-	}
+    public int getStatus() {
+        return status;
+    }
 
-	public void setStatus(int status) {
-		this.status = status;
-	}
+    public void setStatus(int status) {
+        this.status = status;
+    }
 
-	public String getUserID() {
-		return userID;
-	}
+    public String getUserID() {
+        return userID;
+    }
 
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
 
-	public Date getReturnDay() {
-		return returnDay;
-	}
+    public Date getReturnDay() {
+        return returnDay;
+    }
 
-	public void setReturnDay(Date returnDay) {
-		this.returnDay = returnDay;
-	}
+    public void setReturnDay(Date returnDay) {
+        this.returnDay = returnDay;
+    }
 }
--- src/ateam/model/User.java
+++ src/ateam/model/User.java
@@ -1,58 +1,58 @@
 package ateam.model;
 
 public class User {
-	private String userID;
-	private String password;
-	private String userName;
-	private String userKana;
-	private String deptID;
-	private int authority;
+    private String userID;
+    private String password;
+    private String userName;
+    private String userKana;
+    private String deptID;
+    private int authority;
 
-	public String getUserID() {
-		return userID;
-	}
+    public String getUserID() {
+        return userID;
+    }
 
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getUserKana() {
-		return userKana;
-	}
+    public String getUserKana() {
+        return userKana;
+    }
 
-	public void setUserKana(String userKana) {
-		this.userKana = userKana;
-	}
+    public void setUserKana(String userKana) {
+        this.userKana = userKana;
+    }
 
-	public String getDeptID() {
-		return deptID;
-	}
+    public String getDeptID() {
+        return deptID;
+    }
 
-	public void setDeptID(String deptID) {
-		this.deptID = deptID;
-	}
+    public void setDeptID(String deptID) {
+        this.deptID = deptID;
+    }
 
-	public int getAuthority() {
-		return authority;
-	}
+    public int getAuthority() {
+        return authority;
+    }
 
-	public void setAuthority(int authority) {
-		this.authority = authority;
-	}
+    public void setAuthority(int authority) {
+        this.authority = authority;
+    }
 }
