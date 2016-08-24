b28ea5edd067af849e83f475b05c91d1a4c2dec3
htlsne <htlsne@users.noreply.github.com>
2016-08-24 11:28:31 +0900

    UserDAO フォーマット

--- src/ateam/dao/UserDAO.java
+++ src/ateam/dao/UserDAO.java
@@ -5,21 +5,21 @@ import java.sql.SQLException;
 import ateam.model.User;
 
 public class UserDAO {
-	private static UserDAO instance = new UserDAO();
+    private static UserDAO instance = new UserDAO();
 
-	private UserDAO() {}
+    private UserDAO() {
+    }
 
-	public static synchronized UserDAO getInstance() {
-		return instance;
-	}
+    public static synchronized UserDAO getInstance() {
+        return instance;
+    }
 
-	public User getUser(String userID) {
-		String sql = "SELECT *  FROM UserKanri WHERE userID = '" + userID +"';";
-		try {
-			return DBManager.getObject(sql , new UserBeansMapping());
-		}
-		catch (SQLException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+    public User getUser(String userID) {
+        String sql = "SELECT *  FROM UserKanri WHERE userID = '" + userID + "';";
+        try {
+            return DBManager.getObject(sql, new UserBeansMapping());
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
