101217550737543e61423b667b2f8760aa29c5c2
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-08-25 09:52:11 +0900

    リクエストロジック変更

--- src/ateam/logic/RequestLogic.java
+++ src/ateam/logic/RequestLogic.java
@@ -1,11 +1,14 @@
 package ateam.logic;
 
+import java.sql.Date;
+
 import ateam.dao.BihinDAO;
 import ateam.model.Bihin;
 
 public class RequestLogic {
-    public static boolean requestBihin(String bihinID) {
+    public static boolean requestBihin(String bihinID , String userID , Date returnDay) {
         BihinDAO dao = BihinDAO.getInstance();
+        dao.update(bihinID , userID , returnDay);
         Bihin bihin = dao.getBihin(bihinID);
         	if(bihin.getStatus() == 1) {
         		//ステータスが貸出可の場合にのみtureを返す
