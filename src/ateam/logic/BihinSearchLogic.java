884645cdbf0b7fad3f1cda08bebc594c079f6aa4
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-08-25 15:22:10 +0900

    bihinSearchLogic変更

--- src/ateam/logic/BihinSearchLogic.java
+++ src/ateam/logic/BihinSearchLogic.java
@@ -7,19 +7,19 @@ import ateam.model.Bihin;
 
 public class BihinSearchLogic {
     public static List<Bihin> getBihinSearchList(int status) {
-        ateam.dao.BihinDAO dao = BihinDAO.getInstance();
+        BihinDAO dao = BihinDAO.getInstance();
         List<Bihin> list = dao.searchBihin(status);
         return list;
     }
 
     public static List<Bihin> getBihinSearchList(String bihinKana , int status){
-        ateam.dao.BihinDAO dao = BihinDAO.getInstance();
+        BihinDAO dao = BihinDAO.getInstance();
         List<Bihin> list = dao.searchBihin(bihinKana ,status);
         return list;
     }
 
     public static List<Bihin> getBihinSeachList(String bihinKana){
-        ateam.dao.BihinDAO dao = BihinDAO.getInstance();
+        BihinDAO dao = BihinDAO.getInstance();
         List<Bihin> list = dao.searchBihin(bihinKana);
         return list;
     }
