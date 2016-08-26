eb7062c0e14c5be7e9ee6a5289e02bb092311ca8
htlsne <htlsne@users.noreply.github.com>
2016-08-26 14:19:50 +0900

    StringUtil フォーマット

--- src/ateam/util/StringUtil.java
+++ src/ateam/util/StringUtil.java
@@ -1,15 +1,15 @@
 package ateam.util;
 
 public class StringUtil {
-    public static String convertHiraganaToKatakana (String str) {
+    public static String convertHiraganaToKatakana(String str) {
         StringBuffer buf = new StringBuffer();
         for (int i = 0; i < str.length(); i++) {
-          char code = str.charAt(i);
-          if ((code >= 0x3041) && (code <= 0x3093)) {
-            buf.append((char) (code + 0x60));
-          } else {
-            buf.append(code);
-          }
+            char code = str.charAt(i);
+            if ((code >= 0x3041) && (code <= 0x3093)) {
+                buf.append((char) (code + 0x60));
+            } else {
+                buf.append(code);
+            }
         }
         return buf.toString();
     }
