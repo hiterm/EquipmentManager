94fec252e1820dc23be9fe4bd608318903ec2ead
htlsne <htlsne@users.noreply.github.com>
2016-08-26 13:55:40 +0900

    読み仮名検索がひらがなでも入力できるように

--- src/ateam/logic/BihinSearchLogic.java
+++ src/ateam/logic/BihinSearchLogic.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import ateam.dao.BihinDAO;
 import ateam.model.Bihin;
+import ateam.util.StringUtil;
 
 public class BihinSearchLogic {
     public static List<Bihin> getBihinSearchList(int status) {
@@ -14,13 +15,15 @@ public class BihinSearchLogic {
 
     public static List<Bihin> getBihinSearchList(String bihinKana ,String bihinName, int status){
         BihinDAO dao = BihinDAO.getInstance();
-        List<Bihin> list = dao.searchBihin(bihinKana ,bihinName,status);
+        String katakanaBihinKana = StringUtil.convertHiraganaToKatakana(bihinKana);
+        List<Bihin> list = dao.searchBihin(katakanaBihinKana ,bihinName,status);
         return list;
     }
 
     public static List<Bihin> getBihinSeachList(String bihinKana,String bihinName){
         BihinDAO dao = BihinDAO.getInstance();
-        List<Bihin> list = dao.searchBihin(bihinKana,bihinName);
+        String katakanaBihinKana = StringUtil.convertHiraganaToKatakana(bihinKana);
+        List<Bihin> list = dao.searchBihin(katakanaBihinKana ,bihinName);
         return list;
     }
 
