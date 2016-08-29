ac08f22807dd3934ccd558c0189887b9b32a2571
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-08-29 12:32:14 +0900

    LogUtil修正

--- src/ateam/util/LogUtil.java
+++ src/ateam/util/LogUtil.java
@@ -12,7 +12,7 @@ import ateam.model.Bihin;
 import ateam.model.User;
 
 public class LogUtil {
-    public static void createLogDate(String userID, String bihinID) {
+    synchronized public static void createLogDate(String userID, String bihinID) {
         try {
             File file = new File("C:\\pleiades\\pleiades\\workspace\\EquipmentManager\\WebContent\\WEB-INF\\log\\log.txt");
             Date date = new Date();
@@ -25,13 +25,13 @@ public class LogUtil {
             if (bihin.getStatus() == 1) {
                 status = "返却";
                 FileWriter filewriter = new FileWriter(file, true);
-                filewriter.write("操作:"+status+"　ユーザID:"+userID+"　ユーザ名:"+user.getUserName()+"　備品ID:"+bihinID+"　備品名:"+bihin.getBihinName()+"　操作日時:"+sdf.format(date)+"\r\n");
+                filewriter.write("操作日時:"+sdf.format(date)+"　操作:"+status+"　ユーザID:"+userID+"　ユーザ名:"+user.getUserName()+"　備品ID:"+bihinID+"　備品名:"+bihin.getBihinName()+"\r\n");
                 filewriter.close();
             }
             else {
                 status = "貸出";
                 FileWriter filewriter = new FileWriter(file, true);
-                filewriter.write("操作:"+status+"　ユーザID:"+userID+"　ユーザ名:"+user.getUserName()+"　備品ID:"+bihinID+"　備品名:"+bihin.getBihinName()+"　返却予定日:"+bihin.getReturnDay()+"　操作日時:"+sdf.format(date)+"\r\n");
+                filewriter.write("操作日時:"+sdf.format(date)+"　操作:"+status+"　ユーザID:"+userID+"　ユーザ名:"+user.getUserName()+"　備品ID:"+bihinID+"　備品名:"+bihin.getBihinName()+"　返却予定日:"+bihin.getReturnDay()+"\r\n");
                 filewriter.close();
             }
         } catch (IOException e) {
