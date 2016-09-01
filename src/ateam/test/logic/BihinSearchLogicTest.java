d2e130f28851e225f0105cc309d2f623c06f0748
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-09-01 11:49:44 +0900

    BihinSearchLogicTest

--- src/ateam/test/logic/BihinSearchLogicTest.java
+++ src/ateam/test/logic/BihinSearchLogicTest.java
@@ -1,7 +1,18 @@
 package ateam.test.logic;
 
-import junit.framework.TestCase;
+import java.util.List;
+
+import ateam.dao.BihinDAO;
+import ateam.logic.BihinSearchLogic;
+import ateam.model.Bihin;
+import junit.framework.TestCase;;
 
 public class BihinSearchLogicTest extends TestCase{
+    public void testGetBihinSearchList(){
+        BihinDAO dao = BihinDAO.getInstance();
+        List<Bihin> list = dao.searchBihin(1);
+        assertEquals(list.size() ,  BihinSearchLogic.getBihinSearchList(1).size());
+
+    }
 
 }
