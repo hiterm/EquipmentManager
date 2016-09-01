7cd606915bf7f629e6e6f8ad8695df29ff92a149
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-09-01 12:55:52 +0900

    ReturnBihinLogicTest

--- src/ateam/test/logic/ReturnBihinLogicTest.java
+++ src/ateam/test/logic/ReturnBihinLogicTest.java
@@ -1,7 +1,13 @@
 package ateam.test.logic;
 
+import ateam.logic.ReturnBihinLogic;
 import junit.framework.TestCase;
 
 public class ReturnBihinLogicTest extends TestCase{
+    public void testReturnBihin(){
+        boolean bool = ReturnBihinLogic.returnBihin("U001", "B004");
+        assertEquals(true, bool);
+
+    }
 
 }
