ba5ce92cd9db51f70ce5531a0976922d5ada7308
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-09-01 13:12:34 +0900

    junitテスト貸出ロジック修正

--- src/ateam/test/logic/RequestLogicTest.java
+++ src/ateam/test/logic/RequestLogicTest.java
@@ -2,21 +2,22 @@ package ateam.test.logic;
 
 import java.sql.Date;
 
+import ateam.dao.BihinDAO;
 import ateam.logic.RequestLogic;
-import ateam.logic.ReturnBihinLogic;
 import junit.framework.TestCase;
 
 public class RequestLogicTest extends TestCase{
     public void testRequestBihinTrue() {
         String datestr = "2016-12-24";
         Date date = Date.valueOf(datestr);
-        assertEquals(true,RequestLogic.requestBihin("B001", "U001",date ));
-        boolean bool = ReturnBihinLogic.returnBihin("U001", "B001");
+        assertTrue(RequestLogic.requestBihin("B001", "U001",date ));
+        BihinDAO dao = BihinDAO.getInstance();
+        dao.update("U001", "B001");
     }
 
     public void testRequestBihinFalse() {
         String datestr = "2016-12-24";
         Date date = Date.valueOf(datestr);
-        assertEquals(false,RequestLogic.requestBihin("B004", "U001",date ));
+        assertFalse(RequestLogic.requestBihin("B004", "U001",date ));
     }
 }
