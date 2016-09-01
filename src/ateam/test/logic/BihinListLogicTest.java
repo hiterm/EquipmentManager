b078705cfc5d29b0ac3505918045484c6e9a622e
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-09-01 12:24:31 +0900

    junitテストすべての備品表示

--- src/ateam/test/logic/BihinListLogicTest.java
+++ src/ateam/test/logic/BihinListLogicTest.java
@@ -1,7 +1,18 @@
 package ateam.test.logic;
 
+import java.util.List;
+
+import ateam.logic.BihinListLogic;
+import ateam.model.Bihin;
 import junit.framework.TestCase;
 
 public class BihinListLogicTest extends TestCase{
+    public void testGetAllBihinList() {
+        List<Bihin> list =  BihinListLogic.getAllBihinList();
+        assertEquals(100,list.size());
+        assertEquals("B001",list.get(0).getBihinID());
+        assertEquals("B002",list.get(1).getBihinID());
+        assertEquals("B003",list.get(2).getBihinID());
 
+    }
 }
