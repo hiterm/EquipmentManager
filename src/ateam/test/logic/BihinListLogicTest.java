a0c8db159b5a08c950235cbbbefde484ad7ab431
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-09-01 12:48:41 +0900

    junitテストログイン

--- src/ateam/test/logic/BihinListLogicTest.java
+++ src/ateam/test/logic/BihinListLogicTest.java
@@ -9,7 +9,7 @@ import junit.framework.TestCase;
 public class BihinListLogicTest extends TestCase{
     public void testGetAllBihinList() {
         List<Bihin> list =  BihinListLogic.getAllBihinList();
-        assertEquals(100,list.size());
+        assertEquals(15,list.size());
         assertEquals("B001",list.get(0).getBihinID());
         assertEquals("B002",list.get(1).getBihinID());
         assertEquals("B003",list.get(2).getBihinID());
--- src/ateam/test/logic/LoginLogicTest.java
+++ src/ateam/test/logic/LoginLogicTest.java
@@ -1,7 +1,22 @@
 package ateam.test.logic;
 
+import ateam.logic.LoginLogic;
+import ateam.model.User;
 import junit.framework.TestCase;
 
 public class LoginLogicTest extends TestCase{
+    public void testLogin() {
+        User user = LoginLogic.login("U001", "A0000001");
+        assertEquals("U001",user.getUserID());
+        assertEquals("A0000001",user.getPassword());
+        assertEquals("田中",user.getUserName());
+        assertEquals("タナカ",user.getUserKana());
+        assertEquals("D001",user.getDeptID());
+        assertEquals(1,user.getAuthority());
+    }
 
+    public void testLogin2() {
+        User user = LoginLogic.login("U001", "A000");
+        assertEquals(null,user);
+    }
 }
