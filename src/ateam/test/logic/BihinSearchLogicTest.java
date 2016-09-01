3687b7ec3c4b6e53e02d6a7c0bda5fdf84111c2f
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-09-01 12:47:14 +0900

    BihinSearchLogicTest完了

--- src/ateam/test/logic/BihinSearchLogicTest.java
+++ src/ateam/test/logic/BihinSearchLogicTest.java
@@ -16,4 +16,24 @@ public class BihinSearchLogicTest extends TestCase{
 
     }
 
+    public void testGetBihinSearchList2(){
+        List<Bihin> list = BihinSearchLogic.getBihinSearchList("フセン", "フセン", 1);
+        assertEquals(2 , list.size());
+        assertEquals("B007", list.get(0).getBihinID());
+        assertEquals("B008", list.get(1).getBihinID());
+    }
+
+    public void testGetBihinSearchList3(){
+        List<Bihin> list = BihinSearchLogic.getBihinSeachList("フセン", "フセン");
+        assertEquals(3 , list.size());
+        assertEquals("B004", list.get(0).getBihinID());
+        assertEquals("B007", list.get(1).getBihinID());
+        assertEquals("B008", list.get(2).getBihinID());
+    }
+
+    public void testGetStatusSearch(){
+        int status = BihinSearchLogic.getStatusSearch("success");
+        assertEquals(1, status);
+    }
+
 }
