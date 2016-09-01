d563566d36a0f8c51e4206074dee9f8180a44f03
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-09-01 12:51:08 +0900

    MyPageLogicTest

--- src/ateam/test/logic/MyPageLogicTest.java
+++ src/ateam/test/logic/MyPageLogicTest.java
@@ -1,7 +1,17 @@
 package ateam.test.logic;
 
+import java.util.List;
+
+import ateam.logic.MyPageLogic;
+import ateam.model.Bihin;
 import junit.framework.TestCase;
 
 public class MyPageLogicTest extends TestCase{
+    public void testGetBihinList(){
+        List<Bihin> list = MyPageLogic.getBihinList("U001");
+        assertEquals(1 , list.size());
+        assertEquals("B004", list.get(0).getBihinID());
+
+    }
 
 }
