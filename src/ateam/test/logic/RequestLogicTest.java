aef0aee83f5fd4811b9f7461af680f754a933533
mk0703 <ko_hei0703@msn.comko_hei0703@msn.com>
2016-09-01 12:56:24 +0900

    junitテスト貸出ロジック

--- src/ateam/test/logic/RequestLogicTest.java
+++ src/ateam/test/logic/RequestLogicTest.java
@@ -1,7 +1,20 @@
 package ateam.test.logic;
 
+import java.sql.Date;
+
+import ateam.logic.RequestLogic;
 import junit.framework.TestCase;
 
 public class RequestLogicTest extends TestCase{
+    public void testRequestBihinTrue() {
+        String datestr = "2016-12-24";
+        Date date = Date.valueOf(datestr);
+        assertEquals(true,RequestLogic.requestBihin("B001", "U001",date ));
+    }
 
+    public void testRequestBihinFalse() {
+        String datestr = "2016-12-24";
+        Date date = Date.valueOf(datestr);
+        assertEquals(false,RequestLogic.requestBihin("B004", "U001",date ));
+    }
 }
