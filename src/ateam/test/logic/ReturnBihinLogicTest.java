126a6955be13a3b180beea8520bf9b486210b47c
kakamiyuta <kakamiyuta@users.noreply.github.com>
2016-09-01 13:03:48 +0900

    ReturnBihinLogicTestの修正

--- src/ateam/test/logic/ReturnBihinLogicTest.java
+++ src/ateam/test/logic/ReturnBihinLogicTest.java
@@ -1,11 +1,17 @@
 package ateam.test.logic;
 
+import java.sql.Date;
+
+import ateam.logic.RequestLogic;
 import ateam.logic.ReturnBihinLogic;
 import junit.framework.TestCase;
 
 public class ReturnBihinLogicTest extends TestCase{
     public void testReturnBihin(){
-        boolean bool = ReturnBihinLogic.returnBihin("U001", "B004");
+        String datestr = "2016-12-24";
+        Date date = Date.valueOf(datestr);
+        assertEquals(true,RequestLogic.requestBihin("B002", "U002",date ));
+        boolean bool = ReturnBihinLogic.returnBihin("U002", "B002");
         assertEquals(true, bool);
 
     }
